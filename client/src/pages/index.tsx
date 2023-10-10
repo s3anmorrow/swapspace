6c8134d0968d686a005318b3579bee40fb4ecb91
Sean Morrow <s3anmorrow@gmail.com>
2023-10-10 10:34:20 -0300

    end of lesson

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,27 @@
 // importing google font for NextJS
+import { getJSONData } from '@/tools/Toolkit';
 import { Griffy } from 'next/font/google';
 const griffy = Griffy({weight: "400", subsets: ['latin']});
+
+import { Orders } from "@/tools/orders.model";
 
 export default function Home() {
   // retrieve server sided script
   const RETRIEVE_SCRIPT:string = "https://www.seanmorrow.ca/_lessons/retrieveOrder.php";
 
-  
+  // -------------------------------------------- event handlers
+  const onResponse = (data:Orders) => {
+    console.log(data);
+  };
+
+  const onError = (message:string) => {
+    console.log(`*** Error retrieving pizza order data :( | ${message}`);
+  };
+
+  const getOrders = (e:any) => {
+    // fetch the data from the api
+    getJSONData(RETRIEVE_SCRIPT, onResponse, onError);
+  };
 
 
   // ---------------------------- rendering to DOM
@@ -37,7 +52,7 @@ export default function Home() {
           <div className="text-accent text-3xl font-bold mb-2.5">Welcome loyal pizza dispatcher....</div>Click the &quot;Get Orders&quot; button below to view all current orders that need to be delivered.
           <div>
               <button 
-                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5">Get Orders</button>
+                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5" onClick={getOrders}>Get Orders</button>
           </div>
         </div>
         <div className="shrink-0 text-lg text-right text-greyContent hidden md:block">

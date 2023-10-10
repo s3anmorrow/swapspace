8952b3c20543407580a6f93c814555192bed8a46
Sean Morrow <s3anmorrow@gmail.com>
2023-10-10 11:03:50 -0300

    changes

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,17 +1,36 @@
 // importing google font for NextJS
+import { getJSONData } from '@/tools/Toolkit';
 import { Griffy } from 'next/font/google';
 const griffy = Griffy({weight: "400", subsets: ['latin']});
+
+import { Orders } from "@/tools/orders.model";
+import LoadingOverlay from "@/components/LoadingOverlay";
 
 export default function Home() {
   // retrieve server sided script
   const RETRIEVE_SCRIPT:string = "https://www.seanmorrow.ca/_lessons/retrieveOrder.php";
 
-  
+  // -------------------------------------------- event handlers
+  const onResponse = (data:Orders) => {
+    console.log(data);
+  };
+
+  const onError = (message:string) => {
+    console.log(`*** Error retrieving pizza order data :( | ${message}`);
+  };
+
+  const getOrders = (e:any) => {
+    console.log("sending data!");
+    // fetch the data from the api
+    getJSONData(RETRIEVE_SCRIPT, onResponse, onError);
+  };
 
 
   // ---------------------------- rendering to DOM
   return (
     <main className="grid grid-rows-1 grid-cols-1 gap-0 text-content">
+
+      <LoadingOverlay spinnerColor="#FFFFFF" bgColor="#b82308" showSpinner={true} enabled={true} />
 
       <div className="flex flex-nowrap items-center justify-center 
           bg-accent bg-[url('./../lib/images/background.jpg')] bg-no-repeat bg-center bg-cover
@@ -37,7 +56,7 @@ export default function Home() {
           <div className="text-accent text-3xl font-bold mb-2.5">Welcome loyal pizza dispatcher....</div>Click the &quot;Get Orders&quot; button below to view all current orders that need to be delivered.
           <div>
               <button 
-                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5">Get Orders</button>
+                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5" onClick={getOrders}>Get Orders</button>
           </div>
         </div>
         <div className="shrink-0 text-lg text-right text-greyContent hidden md:block">
